e70fde05119c61ab21c8024d0c49f8a1911d320b
Alex <talent@unseen.is>
2017-05-22 20:06:19 -0400

    MPI

---
+++ Monster/Paradox/Hydra/Hydra/app/assets/images/Message_Passing_Galaxy.py
@@ -0,0 +1,152 @@
+#-*- ;)
+#Todo
+# asciiart				  ;)			Dis Be Hooman
+						 #*
+						#* #* #* #*
+					   #* 			
+					  #*
+					 #*
+					  #*
+					   #*
+#Bear				    #*
+						 #*
+						 
+																'''
+              ,.,_,.
+           ,.''     \
+          '          '          F-U-T-U-R-A-M-A
+        /'           |
+      /_-            |    Prof. Hubert Farnsworh
+    .'__      _-_    :
+   /__        _-_    :
+  ,_,._     ,_,._~   |___
+.'-_ '.'.-.'-_ '.'._-^_  '.
+|  -_ |.| |  -_ | | / |
+ ',_,' /  _',_,'_'  /|/
+  .  .|    ',. ._-^  |'
+   ' '.   .'  '.    '/|
+ ,'    '''    __'.  \/ -_
+'_=-..--..--'^  '', : \. '.
+     ',    .  ,   ,' \/ |  |-_
+     / ',.. '. '. ,../  |  |  '-_
+   ,'  . \'.:.''''    .''. '.    \.
+ ,'    | |\       ,../   |  |      ',
+ |     ' ''.,.''''       ', ',       |																	
+																''''''
+#  _________
+# |________|	__      __		 
+# |	 |		   |  \    |  \
+# |  | 		  |    \  |    \
+# |  |       |      \|      \
+# |  |      |    |\     |\   \
+# |  |     |    |  \   |  \   \
+# |__|    |____|    \_|    \___\
+
+#              0000000$$$$0                                                                    ||
+#             00000000     0                                                                   ||
+#            00000000       0                                                                    ||
+#           0000 000@        00                                                                    ||
+#          0000  00$$   $  $   000                                                                   ||
+#           00000$$00    -    0000                                                                    ||
+#            00000000   ()() 00000                                                                    ||
+#             000000000@@@00000000                                                                    ||
+#                                                                              ||
+#                                                                              ||
+#                                                                              ||
+#                                                                              ||
+#                                                                              ||
+#                                                                              ||
+#                                                                              ||
+import abc
+import cmath
+import array
+class __Rescue__(Person,Self,One,LM,Two, EJ):
+	print("To the Rescue")
+	#Class Rescue Is Valid and Working
+	class __Witcher__(Game,Two,Allow,Remote,Access,To,User,File,dir,home,ofTwo):
+		print("Access Granted" + "Remote Access" + "file(__file__(__Rescue__))")
+		file(__Rescue__[assist,mode[buffer,1+1]])
+		simulate(__loader__(__Rescue__(__file__(self))))
+		__Witcher__.downloading = true
+		status = cmath.sqrt.pi(84/100)
+		status = true; games_in_lib - __Witcher__ != setattr;
+		a = ['SYN','ACK','QCK','RCK','MAK','TAK','ATT','SYN','ACK','YAK','OPK','ROK','MOK','SOK']
+		print reversed(a) #print(trace)
+		Adjust[array.array.byteswap(a)]
+		boring = bool(a);a = ReferenceError #good
+		ReferenceError(property)
+		def __property__(self): 
+			reduce(status,a[0,1]);
+			if(status == true):
+				reload(module)
+			#end
+		#end
+		print("\t\s\k /Not[g-N,N-a[g^x(%m[d4%t[{w9y}]])]]/".decode) #Self.Preserved Class Remote Access
+		def __call__(self): #Doctor
+			cmath.acos(abs(vars))
+		#end
+		def __cmp__(self,Doc):
+			cmp(abs,vars)
+			abc.ABCMeta(ABC).register#?
+			abc.abstractmethod(cmp(__call__,__cmp__).conjugate(self))
+			reversed('Godess', 'Empress', 'Mystikal', 'Sorceress')
+		#end
+		#See.Rescue?
+	#end
+#end
+
+'''
+@Todo
+@Credit: http://www.ascii-code.com/ascii-art/video-games/sonic-the-hedgehog.php
+                         ...,?77??!~~~~!???77?<~.... 
+                        ..?7`                           `7!.. 
+                    .,=`          ..~7^`   I                  ?1. 
+       ........  ..^            ?`  ..?7!1 .               ...??7 
+      .        .7`        .,777.. .I.    . .!          .,7! 
+      ..     .?         .^      .l   ?i. . .`       .,^ 
+       b    .!        .= .?7???7~.     .>r .      .= 
+       .,.?4         , .^         1        `     4... 
+        J   ^         ,            5       `         ?<. 
+       .%.7;         .`     .,     .;                   .=. 
+       .+^ .,       .%      MML     F       .,             ?, 
+        P   ,,      J      .MMN     F        6               4. 
+        l    d,    ,       .MMM!   .t        ..               ,, 
+        ,    JMa..`         MMM`   .         .!                .; 
+         r   .M#            .M#   .%  .      .~                 ., 
+       dMMMNJ..!                 .P7!  .>    .         .         ,, 
+       .WMMMMMm  ?^..       ..,?! ..    ..   ,  Z7`        `?^..  ,, 
+          ?THB3       ?77?!        .Yr  .   .!   ?,              ?^C 
+            ?,                   .,^.` .%  .^      5. 
+              7,          .....?7     .^  ,`        ?. 
+                `<.                 .= .`'           1 
+                ....dn... ... ...,7..J=!7,           ., 
+             ..=     G.,7  ..,o..  .?    J.           F 
+           .J.  .^ ,,,t  ,^        ?^.  .^  `?~.      F 
+          r %J. $    5r J             ,r.1      .=.  .% 
+          r .77=?4.    ``,     l ., 1  .. <.       4., 
+          .$..    .X..   .n..  ., J. r .`  J.       `' 
+        .?`  .5        `` .%   .% .' L.'    t 
+        ,. ..1JL          .,   J .$.?`      . 
+                1.          .=` ` .J7??7<.. .; 
+                 JS..    ..^      L        7.: 
+                   `> ..       J.  4. 
+                    +   r `t   r ~=..G. 
+                    =   $  ,.  J 
+                    2   r   t  .; 
+              .,7!  r   t`7~..  j.. 
+              j   7~L...$=.?7r   r ;?1. 
+               8.      .=    j ..,^   .. 
+              r        G              . 
+            .,7,        j,           .>=. 
+         .J??,  `T....... %             .. 
+      ..^     <.  ~.    ,.             .D 
+    .?`        1   L     .7.........?Ti..l 
+   ,`           L  .    .%    .`!       `j, 
+ .^             .  ..   .`   .^  .?7!?7+. 1 
+.`              .  .`..`7.  .^  ,`      .i.; 
+.7<..........~<<3?7!`    4. r  `          G% 
+                          J.` .!           % 
+                            JiJ           .` 
+                              .1.         J 
+                                 ?1.     .'         
+                                     7<..%'''
