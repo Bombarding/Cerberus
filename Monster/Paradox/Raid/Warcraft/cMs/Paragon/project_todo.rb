c46e3cdcb6d4c8de390b1b28dd732037371de927
Alex <talent@unseen.is>
2017-05-24 14:49:44 -0400

    IDEK

--- Monster/Paradox/Raid/Warcraft/cMs/Paragon/project_todo.rb
+++ Monster/Paradox/Raid/Warcraft/cMs/Paragon/project_todo.rb
@@ -3,3 +3,26 @@ def init
 	protect_from_forgery
 	$ADMIN.config.id = false
 end
+
+"""
+import json
+triss = lambda: collections.defaultdictionary(triss)
+root = triss()
+root['Login']['User']['Password'] = '__main__(__file__)'
+root['Menu']['Item']['Value'] = '+__File__+'
+root['Menu']['Cerberus']['New']['Address'] = 'Confirmation'
+root['Menu']['Medusa']['New']['Host'] = 'Confirmation'
+root['Menu']['CookieMonster']['New']['Initialization']['Paradox'] = 'open()'
+root['Menu']['CookieMonster']['New']['Paradox']['Initialization'] = 'send()'
+root['Menu']['Viscious'] = 'new()'
+root['Menu']['Hydra'] = 'new()'
+root['Menu']['Minotaur'] = 'new()'
+root['Menu']['Raid'] = 'uninitialized()'
+root['Raid']['BillyTehKizr'] = 'open()'
+root['Raid']['Deus_Ex_Cerberus'] = 'open()'
+root['Raid']['ShouTucker'] = 'open()'
+root['Menu']['Raid']['Warcraft'] = 'sim()'
+root['Menu']['Raid']['cMs']['Paragon'] = 'close()'
+
+print json.dumps(root,sort_keys=True,indent=30, separators=(',',':'))
+"""
