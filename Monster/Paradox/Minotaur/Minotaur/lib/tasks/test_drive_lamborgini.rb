aa5522de22ea9bd00da14e278f5c29b92b09d4a8
Alex <talent@unseen.is>
2017-06-01 22:21:44 -0400

    Toonage

---
+++ Monster/Paradox/Minotaur/Minotaur/lib/tasks/test_drive_lamborgini.rb
@@ -0,0 +1,58 @@
+'''
+require 'open-uri'
+
+url = URI.parse('http://www.google.com')
+open(url) do |http|
+  response = http.read
+  puts "response: #{response.inspect}"
+end
+'''
+require 'selenium-webdriver'
+#playvideo = Selenium::WebDriver
+#playvideo "http://google.com"
+
+require 'launchy'
+#Launchy.open("https://www.youtube.com/watch?v=RVcCDRzjtWg")
+#Launchy.open("https://www.google.com")
+#init = 'https://www.youtube.com/watch?v=RVcCDRzjtWg'
+#Launchy.open(init)
+
+
+# File lib/launchy.rb, line 25
+def lauch_url*(options:, user);, music:)
+	if user.does(!= music.selcion)
+		self select(try, song, for, enjoyment)
+		begin("self.producion".ljust(@user, self:, create:, $ugk, @@KG:, res[me]:)
+			  [r].all?(in self' self '0)
+		  rescue drink
+			  respond_to?music in select
+			  self place.return
+			  (asd)./(1232)
+end
+		def __initialion__(intervention: retry)
+			class escape(es_cape_eh>)
+				Launchy(open(self(browser(create(application)))))
+			end
+		end
+		
+class art
+	print user
+	def ascii
+		printf watch
+		class ImperialIPA
+			SecurityError(StickFigure)
+			def magruders
+				#8/10 decent
+				#amazing profile for Double IPA
+				#brings back mems
+				"""
+			xxxxxxxx        xxxxxxx 
+					z  x  zz
+					 zz\ zz
+					  zzzz
+				"""
+				tbh.idek
+			end
+		end
+	end
+end
