89095a9dfbb43491d158997207913f7fbf7469e1
Bombarding <skruffy@aim.com>
2017-06-21 23:54:59 -0400

    Kizr.rb
    Week 1 iD Tech Take Home For Students

---
+++ Kizr.rb
@@ -0,0 +1,69 @@
+message = @message = @@message
+@message = _message = @@message
+@@message != message not @Message.chomp?
+message XOR m || M || msg || throw(Exception($Exception)) || throw(SecurityError(m).unpack?)
+#TRUE_MESSAGE = Example: Communications($DEFAULT_ACCESS[ADMINISTRATOR(iD)])
+#TRUE_RESPONSE = Example: Wait.a(acos(PI).second? for he.class in show p communication! end)
+#see.value?
+begin(message.to_k("Kizr".between?(i && u),"omg".ascii_only?(true)))
+    require 'RUN THE PROGRAM YOU WRITE AFTER EVERY WORD<LINE<THOUGHT<CHANGE<ETC'
+    def self p Exception for fork(proc.id("System32.exe")) in Self.System(Login[User/Pass]) where(User = "Username".squeeze!(*self) && Pass = "Password".crypt(1)) end end
+class Infinity def User def Pass end end end
+See.eYe(am["Coding".b(format)for i do care p u + "Learning"])
+c[].message?()
+
+#Welcome To the Wonder World of Ruby
+#Hopefully you have gotten this far and can now open these
+#files up in Komodo and Git Bash.
+def main(welcome)
+    puts "Welcome To My World #{welcome}"
+    puts Time.now
+    #p
+    """
+      ___                       ___           ___     
+     /__/|        ___          /  /\         /  /\    
+    |  |:|       /  /\        /  /::|       /  /::\   
+    |  |:|      /  /:/       /  /:/:|      /  /:/\:\  
+  __|  |:|     /__/::\      /  /:/|:|__   /  /:/~/:/  
+ /__/\_|:|____ \__\/\:\__  /__/:/ |:| /\ /__/:/ /:/___
+ \  \:\/:::::/    \  \:\/\ \__\/  |:|/:/ \  \:\/:::::/
+  \  \::/~~~~      \__\::/     |  |:/:/   \  \::/~~~~ 
+   \  \:\          /__/:/      |  |::/     \  \:\     
+    \  \:\         \__\/       |  |:/       \  \:\    
+     \__\/                     |__|/         \__\/    
+    
+    
+    """
+end
+main("\f\r x^x [z-A] '/b'")
+#Created Main Function. Currently The only Thing that will write to the screen if compiled
+#Correctly. Will print Welcome Message and Time.
+class Colors
+   COLOR1 = "\e[1;36;40m"
+   COLOR2 = "\e[1;35;40m"
+   NOCOLOR = "\e[0m"
+   RED = "\e[1;31;40m"
+   GREEN = "\e[1;32;40m"
+   DARKGREEN = "\e[0;32;40m"
+   YELLOW = "\e[1;33;40m"
+   DARKCYAN = "\e[0;36;40m"
+end
+
+class String
+   def color(color)
+      return color + self + Colors::NOCOLOR
+   end
+end
+
+"Hello World".color(Colors::DARKGREEN)
+=begin
+p "\e[1;31;40m"
+[0, 1, 4, 5, 7].each do |attr|
+  30.upto(37) do |fg|
+    40.upto(47) do |bg|
+      print "\033[#{attr};#{fg};#{bg}m #{fg};#{bg}  "
+    end
+  puts "\033[0m"
+  end
+end
+=end
