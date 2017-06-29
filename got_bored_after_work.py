5e436f5d17e19de9dd3113d255a8faeeb7f42a5a
Bombarding <skruffy@aim.com>
2017-06-28 22:15:56 -0400

    Create got_bored_after_work.py

---
+++ got_bored_after_work.py
@@ -0,0 +1,184 @@
+#Welcoming the user to a self made encryption.
+#regardless of functionality, it must always compile
+#Therefore, be careful what you actually print to the screen
+#or run unless you fully understand what the program is.
+
+import array
+x = ['one','two','three','four','five','six','seven','eight']
+#array.array.append(x)
+#vars.__doc__(enumerate(0, 1)#0xFF
+print("Guess a Letter. You have a 1 in 26 chance of getting this program to run properly on the first run")
+x = eval(input("$"))
+print(x)
+#Wait a second. What did he just do there?
+
+'''Since i Really would like you to run my Ruby Website and Deface(Make Your Own index.html.erb) file
+This'll have to do.
+Source Obtained From Komodo Edit Toolbox.
+3 Bars Top Right > View > Tabs > Toolbox > HTML
+
+<form name="Kizr" action="return ping www.idtech.com" method="POST" enctype="application/x-www-form-urlencoded"><input type="submit"/><input type="reset"/></form>
+<div>
+    <h6>Something Something Dark Side</h6>
+    <li>
+        User: "Username"
+        Password: "Password"
+        CCN: "Credit Card Name"
+        @id: "localhost:3000/kizr"
+        $MACHINE_LOCAL_REG: "Username"
+    </li>
+    <strong>
+        <p style="cursive" class="The Force Is Strong With this one"></p>
+    </strong>
+    <textarea name="Functional Cryptography, But its all in a comment xD" cols="2" rows="1" wrap="SOFT" gift="BOOM"></textarea>
+</div>'''
+#Pics Up The Mic. Alright, Lets get back to Pythonalingus.
+
+kizr='''
+__________                          .___            
+\______   \_____ ____________     __| _/_______  ___
+ |     ___/\__  \\_  __ \__  \   / __ |/  _ \  \/  /
+ |    |     / __ \|  | \// __ \_/ /_/ (  <_> >    < 
+ |____|    (____  /__|  (____  /\____ |\____/__/\_ \
+                \/           \/      \/           \/
+'''
+
+rziK="""
+      ___                       ___           ___     
+     /__/|        ___          /  /\         /  /\    
+    |  |:|       /  /\        /  /::|       /  /::\   
+    |  |:|      /  /:/       /  /:/:|      /  /:/\:\  
+  __|  |:|     /__/::\      /  /:/|:|__   /  /:/~/:/  
+ /__/\_|:|____ \__\/\:\__  /__/:/ |:| /\ /__/:/ /:/___
+ \  \:\/:::::/    \  \:\/\ \__\/  |:|/:/ \  \:\/:::::/
+  \  \::/~~~~      \__\::/     |  |:/:/   \  \::/~~~~ 
+   \  \:\          /__/:/      |  |::/     \  \:\     
+    \  \:\         \__\/       |  |:/       \  \:\    
+     \__\/                     |__|/         \__\/    
+    
+    
+    """
+
+'''
+    #include <iostream>
+    #include <ostream>
+    #include <string>
+    #include <math.h>
+    #include <header.h>
+    #include <self.h>
+    using namespace std;
+    int main(*args, &argsv){for(int i = 0; i < 8; i++){cout<<"Suh Dude";float a,b,c; double kizr, silverrhinosiraptor, Rhinefox; a=2.19;c=3.14;b=4.69; a=kizr; b=silverrhinosiraptor; c=Rhinefox; cout << a,Rhinefox,c << endl;}    return SecurityWarning{e};}
+'''
+
+#now if you read that all as one line, excellent. Try running copying it into a c++ file and running it and tell me what it outputs if anything xD
+
+#class kizr(game):
+    #code
+#self.setattr(user)
+#try:
+    #SecurityError("ThrowException")
+    #except i in x, n:
+    #pass
+    #print(array.ArrayType.byteswap(x))
+#end
+'''def one(self,*):
+    """ docs """
+#end
+print(reversed(x))'''
+#ok so I basically tried to get this whole block working then just commented it out and moved onto something else
+
+import asynchat
+#asynchat.catch_warnings('Warning')
+word = 'orange'
+y = ['red', 'blue', 'red', 'green', 'blue', 'blue']
+#asynchat.deque.rotate(x)
+#asynchat.find_prefix_at_end(x,y)
+for word in ['Kizr', "Cat'n", "Hat'n", 'Paradox', 'Equinox', 'Slaughterhouse@coookiemonster.net']:
+    print(property(x))
+import abc
+world = "Oracle"
+#print(abc.WeakSet.difference(world))
+
+'''
+    %DriveBy.exe = (
+        rat.exe=> executable,file,archive
+        keylog.pdf=> executable,file,archive
+        seq.upd=> non-executable,function,$sys,proxy
+    );
+    #bin/bash
+    for ip in range do pingsweep[127.0.0.1] || tcpdump('proxy.list') as args
+    foreach my $ip (KILL --all) {
+        if (ip.addr = 'localhost:3000') {
+            #code
+        }
+        while (tcpdump = true) {
+            $REMOTE_ACCESS = @sys.proxy.reg
+            open(System32.exe, r/w/x|-f, spoof) or die $MACHINE_LOCAL_REGISTRY;
+        }
+    }
+'''
+#honestly you would be hardpressed to get that to run in perl anyway, but it look cool right? maybe
+range(1,255)
+a = 0
+tar = input("eXe:") #-> 32
+i = [0,1]
+#raise
+"udp".capitalize()
+for a in i: print(Exception())
+
+#usr/bin/env/python/self/class/gen/who/cares/
+#message is in the code, print out stuff for the customer.
+
+for letter in 'K':     # First Example
+   print(rziK)
+'''
+def main(welcome)
+    puts "Welcome To My World #{welcome}"
+    puts Time.now
+    #p
+end
+main("\f\r x^x [z-A] '/b'")
+#Created Main Function. Currently The only Thing that will write to the screen if compiled
+#Correctly. Will print Welcome Message and Time.
+class Colors
+   COLOR1 = "\e[1;36;40m"
+   COLOR2 = "\e[1;35;40m"
+   NOCOLOR = "\e[0m"
+   RED = "\e[1;31;40m"
+   GREEN = "\e[1;32;40m"
+   DARKGREEN = "\e[0;32;40m"
+   YELLOW = "\e[1;33;40m"
+   DARKCYAN = "\e[0;36;40m"
+end
+
+class String
+   def color(color)
+      return color + self + Colors::NOCOLOR
+   end
+end
+
+"Hello World".color(Colors::DARKGREEN)
+=begin
+p "\e[1;31;40m"
+[0, 1, 4, 5, 7].each do |attr|
+  30.upto(37) do |fg|
+    40.upto(47) do |bg|
+      print "\033[#{attr};#{fg};#{bg}m #{fg};#{bg}  "
+    end
+  puts "\033[0m"
+  end
+end
+=end
+'''
+
+import time
+print("Level:", time.clock())
+#woops wrong clock
+#print(time.localtime(%d:%m:%Y %h:%M:%S))
+print(time.strftime("%d-%m-%Y %H:%M:%S"))
+#time.gmtime(time.mktime(time.strptime("2017-06-28 14:04:00","%d-%m-%Y %H:%M:%S"))))
+print("\n\n\n")
+print("x \f 6^x _i \r \asudo bash --all".rstrip('x'))
+print("\n\n\n")
+iter(list()), delattr('exe', rat)
+iter(list()), delattr('pdf', keylog)
